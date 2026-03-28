f7c12de3c8fb70a93f7bff86b617e223ee2a720b
keerthikatamilselvan <tamilselvankeerthika8@gmail.com>
2026-03-28 21:24:32 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,30 @@
+// 1. Sum of two numbers
+const sum = (a, b) => {
+    return a + b;
+};
+console.log("Sum:", sum(5, 3));
+
+// 2. Square of a number
+const square = (n) => {
+    return n * n;
+};
+console.log("Square:", square(4));
+
+// 3. Print Hello World
+const hello = () => {
+    console.log("Hello World!");
+};
+hello();
+
+// 4. Print name with HI
+const greet = (name) => {
+    console.log("HI " + name);
+};
+greet("Keerthaka");
+
+// 5. Multiplication
+const multiply = (a, b) => {
+    let result = a * b;
+    console.log("Multiplication:", result);
+};
+multiply(4, 5);
